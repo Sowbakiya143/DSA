7e56e3e56bd8996d430e93f7f22717e75fa7e98b
Sowbakiya <sowbakiya1403@gmail.com>
2026-03-24 11:05:13 +0530

    Create brute.java

---
+++ Arrays/largest_ele/brute.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{ 
+   
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int[] arr={11,54,21,2,3};
+		int n=arr.length;
+		Arrays.sort(arr);
+	    
+		System.out.println(arr[n-1]); 
+		
+
+	}
+}
