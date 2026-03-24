a7f02ed0af171055f0276ebbdfa5cfe8b17b734b
Sowbakiya <sowbakiya1403@gmail.com>
2026-03-24 12:50:25 +0530

    Add optimal reverse method for left rotation

---
+++ Arrays/left-rotate_dplace/optimal-reverse.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{ 
+   public static void reverse(int[] arr,int start,int end){
+       while(start < end){
+           int temp=arr[start];
+           arr[start]=arr[end];
+           arr[end]=temp;
+           start++;
+           end--;
+       }
+   }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int[] arr={1,2,3,4,5};
+		int n=arr.length;
+		
+		int d=2;
+	    d=d%n;
+	    reverse(arr,0,d-1);
+	    reverse(arr,d,n-1);
+	    reverse(arr,0,n-1);
+	    System.out.println(Arrays.toString(arr));
+	}
+}
