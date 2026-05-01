eb1c6dddc1379f4f6ef9338304602dfb71a1a1f8
Sowbakiya <sowbakiya1403@gmail.com>
2026-05-01 20:57:03 +0530

    Create insert_last.java

---
+++ linkedlist/insertion/insert_last.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class node{
+        int data;
+        node next;
+        node(int data1, node next1){
+            this.data=data1;
+            this.next=next1;
+        }
+        node(int data1){
+            this.data=data1;
+            this.next=null;
+        }
+    }
+class Codechef
+{
+    public static void print(node head){
+        node temp=head;
+        while(temp != null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    public static node convert(int[] arr ){
+        node head=new node(arr[0]);
+        node mover=head;
+        for(int i=1;i<arr.length;i++){
+            node temp=new node(arr[i]);
+            mover.next=temp;
+            mover=temp;
+        }
+        return head;
+    }
+    public static node insertlast(node head,int val){
+        node temp= head;
+        while(temp.next != null){
+            temp=temp.next;
+        }
+        node aa=new node(val);
+        temp.next=aa;
+        return head;
+    
+}
+   
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int[] arr={2,3,4,5};
+		node y= convert(arr);
+		node insert=insertlast(y,15);
+		print(insert);
+
+	}
+}
